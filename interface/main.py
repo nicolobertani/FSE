2f5c98a2c60527383f87d4972e8d5a77b910f345
mathleng <57401580+mathleng@users.noreply.github.com>
2023-05-02 12:09:03 +0100

    updated UI

--- interface/main.py
+++ interface/main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QVBoxLayout
 from PyQt5.QtChart import QChart, QChartView, QPieSeries
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QFont
@@ -8,6 +8,9 @@ from PyQt5 import QtCore
 
 class MyWindow(QMainWindow):
     def __init__(self):
+        """
+        initialize the main window
+        """
         super(MyWindow, self).__init__()
         self.xpos = 0
         self.ypos = 0
@@ -22,24 +25,32 @@ class MyWindow(QMainWindow):
         self.initUI()
 
     def createCharts(self):
+        """
+        create the 2 charts
+        :return:
+        """
         x1, x2, x3, x4 = self.lottery[self.questionCount]
         self.series1 = QPieSeries()
         self.series1.append("{}".format(x1), 100)
         self.chart1 = QChart()
         self.chart1.addSeries(self.series1)
-        self.chart1.setTitle("Option 1")
-        self.chart1.setTitleFont(QFont("Arial", 16))
+        self.chart1.setTitle("Sure Option")
+        self.chart1.setTitleFont(QFont("Arial", 20))
 
         self.series2 = QPieSeries()
         self.series2.append("{}".format(x2), x3)
         self.series2.append("{}".format(x4), 100-x3)
         self.chart2 = QChart()
         self.chart2.addSeries(self.series2)
-        self.chart2.setTitle("Option 2")
-        self.chart2.setTitleFont(QFont("Arial", 16))
+        self.chart2.setTitle("Lottery")
+        self.chart2.setTitleFont(QFont("Arial", 20))
 
 
     def updateCharts(self):
+        """
+        updates the charts
+        :return:
+        """
         print(self.questionCount)
         x1, x2, x3, x4 = self.lottery[self.questionCount]
         self.series1.clear()
@@ -53,37 +64,142 @@ class MyWindow(QMainWindow):
         self.chart1.update()
         self.chart2.update()
 
-    def initUI(self):
-        self.sentence = QtWidgets.QLabel()
+    def finished(self):
+        """
+        cleans the main window and shows end message
+        :return:
+        """
+        centralWidget = self.centralWidget()
+        if centralWidget is not None:
+            centralWidget.deleteLater()
+        self.messageFinished = QtWidgets.QLabel("The experiment is over, thank you for your help !")
+        self.messageFinished.setAlignment(QtCore.Qt.AlignCenter)
+        self.messageFinished.setFont(QFont("Arial", 20))
+        messageLayout = QVBoxLayout()
+        messageLayout.addWidget(self.messageFinished)
+        messageWidget = QWidget()
+        messageWidget.setLayout(messageLayout)
+        self.setCentralWidget(messageWidget)
+
+    def toggleOption1(self):
+        """
+        toggle the state of the button for the 1st option
+        :return:
+        """
+        self.option1Clicked = (self.option1Clicked + 1) % 2
+
+    def toggleOption2(self):
+        """
+        toggle the state of the button for the 2nd option
+        :return:
+        """
+        self.option2Clicked = (self.option2Clicked + 1) % 2
+
+    def resetButtons(self):
+        """
+        reset the states of the 2 option buttons
+        :return:
+        """
+        self.option1Clicked = False
+        self.option2Clicked = False
+        self.option1.setStyleSheet(self.buttonStyleOff)
+        self.option2.setStyleSheet(self.buttonStyleOff)
+
+    def confirmed(self):
+        """
+        handles the states when the choice has been confirmed
+        :return:
+        """
+        if self.option2Clicked or self.option1Clicked:
+            self.updateText()
+            self.update()
+            self.updateCharts()
+            self.resetButtons()
+            if self.questionCount <= 11:
+                self.questionCount += 1
+            else:
+                self.finished()
+
+
+    def createButtons(self):
+        """
+        creates the different buttons
+        :return:
+        """
+        self.buttonStyleOff = """
+            QPushButton {
+            background-color: white;
+            border: none;
+            border-radius: 10px;
+            padding: 10px;
+        }
+        """
+
+        self.buttonStyleOn = """
+            QPushButton {
+            background-color: grey;
+            border: none;
+            border-radius: 10px;
+            padding: 10px;
+        }
+        """
+        fontButtons = QFont()
+        fontButtons.setPointSize(15)
+
         self.option1 = QtWidgets.QPushButton(self)
         self.option1.clicked.connect(self.clickedOption1)
+        self.option1Clicked = False
+        self.option1.setFont(fontButtons)
         self.option2 = QtWidgets.QPushButton(self)
         self.option2.clicked.connect(self.clickedOption2)
-        #self.result = QtWidgets.QLabel(self)
-        #self.result.setText("No result yet")
+        self.option2Clicked = False
+        self.option2.setFont(fontButtons)
 
+        self.option1.setStyleSheet(self.buttonStyleOff)
+        self.option2.setStyleSheet(self.buttonStyleOff)
+
+        self.confirm = QtWidgets.QPushButton(self)
+        self.confirm.clicked.connect(self.confirmed)
+        self.confirm.setText("Confirm choice")
+        self.confirm.setStyleSheet(self.buttonStyleOff)
+        self.confirm.setFont(fontButtons)
+
+    def initUI(self):
+        """
+        initialize all the UI elements
+        :return:
+        """
+        font = QFont()
+        font.setPointSize(25)
+        # Set the new font on the label
+        self.sentence = QtWidgets.QLabel()
+        self.sentence.setFont(font)
+        self.createButtons()
 
         self.widget = QWidget()
         self.layout = QGridLayout()
         self.widget.setLayout(self.layout)
         self.layout.addWidget(self.sentence, 0, 0, 1, 3, QtCore.Qt.AlignCenter)
-        self.layout.addWidget(QChartView(self.chart1), 1, 0)
-        self.layout.addWidget(QChartView(self.chart2), 1, 2)
-        self.layout.addWidget(self.option1, 2, 0)
-        self.layout.addWidget(self.option2, 2, 2)
-        #self.layout.addWidget(self.result, 3, 1)
+        self.layout.addWidget(QChartView(self.chart1), 2, 0)
+        self.layout.addWidget(QChartView(self.chart2), 2, 2)
+        self.layout.addWidget(self.option1, 1, 0)
+        self.layout.addWidget(self.option2, 1, 2)
+        self.layout.addWidget(self.confirm, 3, 1)
         self.layout.setRowStretch(0, 1)
         self.layout.setRowStretch(1, 3)
         self.layout.setRowStretch(2, 3)
-        #self.layout.setRowStretch(3, 1)
+        self.layout.setRowStretch(3, 1)
         self.setCentralWidget(self.widget)
         self.updateText()
 
 
 
 
-
     def updateText(self):
+        """
+        updates the different texts
+        :return:
+        """
         x1, x2, x3, x4 = self.lottery[self.questionCount]
         x5 = 100 - int(x3)
         self.sentence.setText(self.sentence_string.format(x1, x2, x3, x4))
@@ -93,24 +209,33 @@ class MyWindow(QMainWindow):
 
 
     def clickedOption1(self):
-        self.updateText()
-        #self.result.setText("You chose the first option")
-        self.update()
-        self.updateCharts()
-        if self.questionCount <= 11:
-            self.questionCount += 1
+
+
+
+        self.toggleOption1()
+        if self.option1Clicked:
+            self.option1.setStyleSheet(self.buttonStyleOn)
+            if self.option2Clicked:
+                self.toggleOption2()
+                self.option2.setStyleSheet(self.buttonStyleOff)
+        else:
+            self.option1.setStyleSheet(self.buttonStyleOff)
 
     def clickedOption2(self):
-        self.updateText()
-        #self.result.setText("You chose the second option")
-        self.update()
-        self.updateCharts()
+
         if self.questionCount <= 11:
             self.questionCount += 1
+        self.toggleOption2()
+        if self.option2Clicked:
+            self.option2.setStyleSheet(self.buttonStyleOn)
+            if self.option1Clicked:
+                self.toggleOption1()
+                self.option1.setStyleSheet(self.buttonStyleOff)
+        else:
+            self.option2.setStyleSheet(self.buttonStyleOff)
 
     def update(self):
         self.sentence.adjustSize()
-        #self.result.adjustSize()
         self.option1.adjustSize()
         self.option2.adjustSize()
 
@@ -129,7 +254,7 @@ class MyWindow(QMainWindow):
         choice12 = (20.9, 120, 7, 10)
         choice13 = (42.9, 120, 33, 10)
 
-        self.sentence_string = "Do you prefer to win for sure {} or play the lottery and win {} with {}% chances or {} if you lose the lottery"
+        self.sentence_string = "Do you prefer to win for sure <b>{} </b> or play the lottery and win <b>{} </b> with <b>{}% chances </b> or <b>{} </b> if you lose the lottery"
         self.lottery = [choice1, choice2, choice3, choice4, choice5, choice6, choice6, choice7, choice8, choice8, choice9, choice10, choice11, choice12, choice13]
 
 
