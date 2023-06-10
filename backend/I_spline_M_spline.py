4f9e6bb2413d27056319700afeae259ba322c34d
mathleng <57401580+mathleng@users.noreply.github.com>
2023-06-10 11:18:14 +0100

    further tests on I spline, Issue in M_basis

--- backend/I_spline_M_spline.py
+++ backend/I_spline_M_spline.py
@@ -16,9 +16,40 @@ def M_knots_sequence(k, interior_knots, boundary_knots=(0, 1)):
     t = [min(boundary_knots)] * k + interior_knots + [max(boundary_knots)] * k
     return t
 
-def M_basis(i, x, k, t):
+def M_basis_vect(i, x, k, t, verbose=True): # @TODO attempt of vectorized function, same way as I-basis
+
+    knot_seq_len = len(t)
+
+    if i + k > knot_seq_len:
+        raise ValueError("i + k > |t|.")
+
+    # force np.array on x and t
+    x = np.array(x)  # Convert x to a NumPy array if it's not already
+    t = np.array(t)  # Convert t to a NumPy array if it's not already
+
+    # if x == 1, reduce by epsilon amount for finding j
+    x[x == 1] = 1 - np.finfo(np.float32).eps
+    print("t", t)
+    if verbose:
+        print('x', x)
+
+    def M_basis_value(x_):
+        out = 0
+        if k == 1:
+            if x_ >= t[i] and x_ < t[i]:
+                out = 1 / (t[i + 1] - t[i])
+
+        elif t[i + k - 1] > t[i-1]:
+            (k * ((x_ - t[i]) * M_basis(i, x_, k - 1, t) + (t[i + k] - x_) * M_basis(i + 1, x_, k - 1, t))) / ((k - 1) * (t[i + k] - t[i]))
+        return out
+
+    print("error", map(M_basis_value, x))
+    return np.array(list(map(M_basis_value, x)))
+
+
+def M_basis(i, x, k, t): # Version used until now
     # be careful with indexes, ≠ than in R
-    if ( i + k > len(t)):
+    if (i + k > len(t)):
         raise ValueError("i + k > |t|.")
     if x == 1:
         x -= 1 * 10 ** -8
@@ -31,6 +62,7 @@ def M_basis(i, x, k, t):
         if t[i+k] > t[i]:
             out = k * ((x - t[i]) * M_basis(i, x, k - 1, t) + (t[i + k] - x) * M_basis(i + 1, x, k - 1, t))  / ((k - 1) * (t[i + k] - t[i]))
     return out
+
 
 def M_spline( k, interior_knots, individual = False, boundary_knots = (0,1),  lambdas = None, x=0):
     t = M_knots_sequence(k, interior_knots, boundary_knots)
@@ -54,20 +86,18 @@ def M_spline( k, interior_knots, individual = False, boundary_knots = (0,1),  la
         out = np.apply_along_axis(lambda i: M_basis(i, x, k, t), axis=0, arr=np.arange(1, m+1)).dot(lambdas)
     return out
 
-
-
 def I_knots_sequence(k, interior_knots, boundary_knots = (0,1)):
     t = [min(boundary_knots)] * (k+1) + interior_knots + [max(boundary_knots)] * (k+1)
     return t
 
 
-def I_basis(i, x, k, t, verbose = False): # @ TODO here is the issue, I want to vectorize it but i am not sure how to it
-    
+def I_basis(i, x, k, t, verbose=True):
+
     knot_seq_len = len(t)
 
     if not (i in range(1, knot_seq_len-k+1)):
         raise ValueError("i > m = length(t) - k.")
-    
+
 
     # force np.array on x and t
     x = np.array(x)  # Convert x to a NumPy array if it's not already
@@ -78,25 +108,27 @@ def I_basis(i, x, k, t, verbose = False): # @ TODO here is the issue, I want to 
     if verbose:
         print('x', x)
 
-    # find indicator j such that t_j <= x < t_{j+1} 
-    j = np.array(list(map(lambda xx : np.where((xx >= t[:-1]) & (xx < t[1:]))[0][0], x))) + 1
+    # find indicator j such that t_j <= x < t_{j+1}
+    j = np.array(list(map(lambda xx: np.where((xx >= t[:-1]) & (xx < t[1:]))[0][0], x))) + 1
     if verbose:
         print('j', j)
 
     def I_basis_value(jx):
         j, x = jx
         print(j, x)
-        
-        if i > j : 
+
+        if i > j:
             out = 0
         elif i < j - k + 1:
             out = 1
         else:
             #### THIS SEEMS RIGHT BUT NEEDS MORE CHECKS
             out = sum(map(
-                lambda ii : (t[ii + k] - t[ii - 1]) / (k + 1) * M_basis(ii - 1, x = x, k = k + 1, t = t), 
+                lambda ii : (t[ii + k] - t[ii - 1]) / (k + 1) * M_basis(ii - 1, x=x, k=k + 1, t=t), #### @TODO I think that the error comes from here
                 range(i, j + 1)))
-        
+            if out == 0.2736: ### @TODO 1st value that shouldnt appear in the array.
+                print("issue")
+        print("j_out", j, out)
         return out
 
     return np.array(list(map(I_basis_value, zip(j, x))))
@@ -117,7 +149,7 @@ def I_spline(k, interior_knots, x=0, lambdas=None, individual=False, boundary_kn
         lambdas = [1/m for _ in range(m)]
     if sum(lambdas) < .9999 or sum(lambdas) > 1.0001:
         warnings.warn("Lambdas do not sum up to 1.")
-    i_sequence = 2 if exclude_constant_splines else 1, m+1 if exclude_constant_splines else m
+    i_sequence = 2 if exclude_constant_splines else 1, m+2 if exclude_constant_splines else m+1
     i_sequence = [i for i in range(i_sequence[0], i_sequence[1])]
 
     if individual:
@@ -142,6 +174,8 @@ y = np.array([min(max(1.2 * xi + np.random.normal(0, 0.2), 0), 1) for xi in x])
 e = 1e-12
 
 
-print("output", I_basis(2, x, 3, (0,0,0,0,.1,.5,.9,1,1,1,1), verbose=True))
+#print("output", I_basis(2, x, 3, (0,0,0,0,.1,.5,.9,1,1,1,1), verbose=True))
+#print("output", [I_basis(i, x, 3, [0, 0, 0, 0, 0.1, 0.5, 0.9, 1, 1, 1, 1]) for i in range(1, 9)])
+#print("output",I_spline(x=x, k=3, interior_knots=[.1, .5, .9], individual = True))
+print("output M_basis test 1", M_basis(6, x, 3, (0, 0, 0, .1, .5, .9, 1, 1, 1)))
 
-#I_spline(x=x, k=3, interior_knots=[.1, .5, .9], individual = True)
