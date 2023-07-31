f0c1fd42e34ede7ab846a17b96f5fff989c1bacf
nicolobertani <29360000+nicolobertani@users.noreply.github.com>
2023-07-31 12:34:04 +0200

    fixed LPs in run_FSE. script is commented to check I_spline values

--- backend/run_FSE.py
+++ backend/run_FSE.py
@@ -8,7 +8,7 @@ import scipy.optimize as opt
 
 
 order = 3
-helper = np.arange(0.00, 1.0, 0.01)
+helper = np.arange(0.01, .99, 0.01)
 chosen_xi = [.1, .9]
 x = 120
 y = 10
@@ -25,19 +25,21 @@ def run_FSE(epsilon_threshold=0.1):
     iteration = 0
     # initialization for LPs
     A1 = np.zeros((m, m))
-    np.fill_diagonal(A1, 1)
+    np.fill_diagonal(A1, -1)
     # bounds with no answers
-    print("helper", helper)
-    print("order", order)
-    print("chosen_xi", chosen_xi)
-    print("m", m)
+    # print("helper", helper)
+    # print("order", order)
+    # print("chosen_xi", chosen_xi)
+    # print("m", m)
+    print("A1", A1)
     lower_bound = I_spline(x=helper, k=order, interior_knots=chosen_xi, individual=True)[m-1]
     upper_bound = I_spline(x=helper, k=order, interior_knots=chosen_xi, individual=True)[0]
     D = upper_bound - lower_bound
     # create storage for bounds
     bound_list = [np.vstack((lower_bound, upper_bound))]
 
-    while epsilon > epsilon_threshold:
+    # while epsilon > epsilon_threshold:
+    while iteration < 1:
         iteration += 1
 
         # find next p
@@ -61,6 +63,8 @@ def run_FSE(epsilon_threshold=0.1):
         # asks the question and records the truth
         sim_answers[iteration-1, 3] = 0 # @TODO check that correct because hard coded #defining_function(sim_answers[iteration, 0]) < sim_answers[iteration, 2]
         sim_answers[iteration-1, 4] = 1 if sim_answers[iteration-1, 3] else -1
+        
+        print(sim_answers)
 
         # prepare parameters for LPs
         if iteration == 1:
@@ -68,127 +72,94 @@ def run_FSE(epsilon_threshold=0.1):
         else:
             A2 = np.transpose(sim_answers[:, 4].reshape(-1, 1) * I_spline(x=sim_answers[:, 0], k=order, interior_knots=chosen_xi, individual=True))
         print("A2", A2) # the signs are the opposite
-        b = np.concatenate(([1], np.zeros(m), sim_answers[:, 4] * sim_answers[:, 2]))
-        constraint_signs = np.concatenate((["eq"], np.repeat("ineq", m), np.repeat("ineq", sim_answers.shape[0])))
-        print("A1", A1)
-        print("1_m", np.repeat(1, m))
-        A = np.column_stack((np.repeat(1, m), A1, A2)).T
+        A = np.column_stack((A1, A2)).T
         print("A", A)
-        print("A.T", A.T)
+        print("A", A.ndim)
+        b = np.concatenate((np.zeros(m), sim_answers[:, 4] * sim_answers[:, 2]))
+        print("b", b)
 
-        #### test
-
-
-        constraint_signs = ["=="] + [">="] * m + ["<="] * sim_answers.shape[0]
-
-        #lower_bound = np.zeros(helper.shape[0])  # Initialize the lower_bound array
-
-        for i, local_x in enumerate(helper):
-            print("local_x", local_x)
-            print(chosen_xi)
-
-            c = I_spline(x=local_x, k=3, interior_knots=chosen_xi, individual=True) # the results of c are a bit different, but very close @ TODO check thats the error
-            print("c", c)
-            sol = opt.linprog(
-            c=c,
-            A_eq= [A[0]],
-            A_ub=np.array([A[i+1] * (-1 if sign == ">=" else 1) for i, sign in enumerate(constraint_signs[1:])]),
-            b_eq=b[0],
-            b_ub= np.array([b[i+1] * (-1 if sign == ">=" else 1) for i, sign in enumerate(constraint_signs[1:])]),
-            method="highs",
-            bounds=(None, None),
-            options={"presolve": False, "sparse": True}
-            )
-            lower_bound[i] = np.dot(sol.x, c)
-
-            #### print to help
-
-            A_eq= A[0],
-            A_ub= np.array([A[i+1] * (-1 if sign == ">=" else 1) for i, sign in enumerate(constraint_signs[1:])])
-            b_eq= [b[0]]
-            b_ub= np.array([b[i+1] * (-1 if sign == ">=" else 1) for i, sign in enumerate(constraint_signs[1:])])
-            print("LP Formulation:")
-            print("Minimize:", end=" ")
-            for i, coeff in enumerate(c):
-                print(f"{coeff}x{i+1}", end=" ")
-            print()
-
-            print("Subject to:")
-
-            # Equality constraints
-            for i, constraint in enumerate(A_eq):
-                print("  ", end="")
-                for j, coeff in enumerate(list(constraint)):
-                    print(f"{coeff}x{j+1}", end=" ")
-                print("=", b_eq[i])
-
-            # Inequality constraints
-            for i, constraint in enumerate(A_ub):
-                print("  ", end="")
-                for j, coeff in enumerate(constraint):
-                    print(f"{coeff}x{j+1}", end=" ")
-                print("<=", b_ub[i])
-
-            print("Bounds:")
-            for i, coeff in enumerate(c):
-                print(f"  x{i+1} >= 0")
-
-            print("results", sol.x)
-
-            # Print or use the lower_bound array as needed
-        print("lower_bound", lower_bound)
-        print("sim answers", sim_answers)
-        print("stop")
+        print("I_spline .1",
+            I_spline(x = .1, k = 3, interior_knots = chosen_xi, individual = True) # the results of c are a bit different, but very close @ TODO check thats the error
+        )
+        print("I_spline .5",
+            I_spline(x = .5, k = 3, interior_knots = chosen_xi, individual = True) # the results of c are a bit different, but very close @ TODO check thats the error
+        )
+        print("I_spline .8",
+            I_spline(x = .8, k = 3, interior_knots = chosen_xi, individual = True) # the results of c are a bit different, but very close @ TODO check thats the error
+        )
+        print("I_spline",
+            I_spline(x = np.linspace(0, 1, 11), k = 3, interior_knots = chosen_xi, individual = True) # the results of c are a bit different, but very close @ TODO check thats the error
+        )
 
 
-        #### issue after that --> NORMAL
 
+    #     for i, local_x in enumerate(helper):
+    #     # for i, local_x in enumerate([.5]):
+            
+    #         c = np.array(
+    #             I_spline(x = local_x, k = 3, interior_knots = chosen_xi, individual = True) # the results of c are a bit different, but very close @ TODO check thats the error
+    #         )
+            
+    #         print("c", c)
 
-        # calculate lower bound
-        lower_bound = np.apply_along_axis(lambda local_x: minimize(lambda c: np.dot(c, I_spline(x=local_x, k=3, interior_knots=chosen_xi, individual=True)),
-                                                                   np.zeros(m), bounds=[(None, None)] * m,
-                                                                   constraints={'type': constraint_signs, 'fun': lambda c: np.dot(c, A.T) - b}).fun,
-                                          axis=0, arr=helper) # minimization
+    #         min_problem = opt.linprog(
+    #             c = c,
+    #             A_ub = A,
+    #             b_ub = b,
+    #             A_eq = np.array([np.array([1] * m)]),
+    #             b_eq = np.array([1])
+    #         )
+    #         lower_bound[i] = np.dot(min_problem.x, c)
 
-        # calculate upper bound
-        upper_bound = np.apply_along_axis(lambda local_x: minimize(lambda c: -np.dot(c, I_spline(x=local_x, k=order, interior_knots=chosen_xi, individual=True)),
-                                                                   np.zeros(m), bounds=[(None, None)] * m,
-                                                                   constraints={'type': constraint_signs, 'fun': lambda c: np.dot(c, A.T) - b}).fun,
-                                          axis=0, arr=helper) # maximization
+    #         max_problem = opt.linprog(
+    #             c = -c,
+    #             A_ub = A,
+    #             b_ub = b,
+    #             A_eq = np.array([np.array([1] * m)]),
+    #             b_eq = np.array([1])
+    #         )
+    #         upper_bound[i] = np.dot(max_problem.x, c)
 
-        # calculate max difference based on updated bounds
-        D = upper_bound - lower_bound
-        epsilon = np.max(D)
+        
+    #     # Print or use the lower_bound array as needed
+    #     print("lower_bound", lower_bound)
+    #     print("upper_bound", upper_bound)
+    #     print("sim answers", sim_answers)
 
-        # store bounds
-        bound_list.append(np.vstack((lower_bound, upper_bound)))
+    #     # calculate max difference based on updated bounds
+    #     D = upper_bound - lower_bound
+    #     epsilon = np.max(D)
 
-    # representative shape ----------------------------------------------------
+    #     # store bounds
+    #     bound_list.append(np.vstack((lower_bound, upper_bound)))
 
-    sim_answers[:, 3] = sim_answers[:, 4]
-    sim_answers[:, 4] = sim_answers[:, 4]
-    sim_answers[:, 5] = sim_answers[:, 4] == sim_answers[:, 5]
-    X = sim_answers[:, 2] - I_spline(x=sim_answers[:, 0], k=order, interior_knots=chosen_xi, individual=True)
-    X = X * sim_answers[:, 3]
-    # setup of constraints
-    D = np.zeros((X.shape[1], X.shape[1]))
-    np.fill_diagonal(D, 1)
-    positive_constraint_matrix = 1 * D
-    d = np.zeros(np.unique(D.shape))
-    A = np.hstack((positive_constraint_matrix, np.transpose(X)))
-    b = np.concatenate((np.repeat(0, positive_constraint_matrix.shape[1]), np.repeat(1, X.shape[0])))
-    # solution
-    res = linprog(c=d, A_ub=A, b_ub=b, method='simplex')
-    normalized_solution = res.x / np.sum(res.x)
-    lambda_constraints = res.slack[0:m] > 0
-    support_vectors = res.slack[(m+1):] > 0
+    # # # representative shape ----------------------------------------------------
 
-    # output ------------------------------------------------------------------
+    # # sim_answers[:, 3] = sim_answers[:, 4]
+    # # sim_answers[:, 4] = sim_answers[:, 4]
+    # # sim_answers[:, 5] = sim_answers[:, 4] == sim_answers[:, 5]
+    # # X = sim_answers[:, 2] - I_spline(x=sim_answers[:, 0], k=order, interior_knots=chosen_xi, individual=True)
+    # # X = X * sim_answers[:, 3]
+    # # # setup of constraints
+    # # D = np.zeros((X.shape[1], X.shape[1]))
+    # # np.fill_diagonal(D, 1)
+    # # positive_constraint_matrix = 1 * D
+    # # d = np.zeros(np.unique(D.shape))
+    # # A = np.hstack((positive_constraint_matrix, np.transpose(X)))
+    # # b = np.concatenate((np.repeat(0, positive_constraint_matrix.shape[1]), np.repeat(1, X.shape[0])))
+    # # # solution
+    # # res = linprog(c=d, A_ub=A, b_ub=b, method='simplex')
+    # # normalized_solution = res.x / np.sum(res.x)
+    # # lambda_constraints = res.slack[0:m] > 0
+    # # support_vectors = res.slack[(m+1):] > 0
 
-    return [sim_answers, np.vstack(bound_list), {"normalized_solution": normalized_solution,
-                                                 "lambda_constraints": lambda_constraints,
-                                                 "support_vectors": support_vectors},
-            bound_list]
+    # # # output ------------------------------------------------------------------
+
+    # # return [sim_answers, np.vstack(bound_list), {"normalized_solution": normalized_solution,
+    # #                                              "lambda_constraints": lambda_constraints,
+    # #                                              "support_vectors": support_vectors},
+    # #         bound_list]
+    # return(bound_list)
 
 
 run_FSE()
