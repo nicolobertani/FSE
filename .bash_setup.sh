92199765dfb39126cc5425ed1ed25e1cd8a91914
nicolobertani <29360000+nicolobertani@users.noreply.github.com>
2025-01-13 14:31:18 +0000

    another attempt with Aptfile

---
+++ .bash_setup.sh
@@ -0,0 +1,3 @@
+# heroku create fse-horserace-experiment
+# heroku buildpacks:add --index 1 heroku/ffmpeg --app fse-horserace-experiment
+heroku buildpacks:add --index 1 https://github.com/heroku/heroku-buildpack-apt
