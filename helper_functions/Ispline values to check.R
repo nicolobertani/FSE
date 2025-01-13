ba44ff9b4ccaf5c1109b3d64451e9b021c6d0691
nicolobertani <29360000+nicolobertani@users.noreply.github.com>
2025-01-13 10:01:21 +0000

    deleted redundant files

--- helper_functions/Ispline values to check.R
+++
@@ -1,7 +0,0 @@
-source("M-spline and I-spline.R")
-
-I_spline(.1, k = 3, interior_knots = c(.1, .9), individual = TRUE)
-I_spline(.5, k = 3, interior_knots = c(.1, .9), individual = TRUE)
-I_spline(.8, k = 3, interior_knots = c(.1, .9), individual = TRUE)
-
-I_spline(seq(0, 1, .1), k = 3, interior_knots = c(.1, .9), individual = TRUE)
--- helper_functions/helper_quantities.R
+++
@@ -1,6 +0,0 @@
-order <- 3
-helper <- seq(.01, .99, by = .01)
-chosen.xi <- c(.1, .9)
-x <- 120
-y <- 10
-
